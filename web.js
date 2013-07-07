d6bafbbfe4df0347e0351b7eb74ad5d3170ede52
Pranav Shrestha <pranavshrestha@yahoo.com>
2013-07-07 22:07:48 +0000

    Added function to read index.html and use it in web.js

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 var fileName = "index.html";
-var buffer = new Buffer(25);
 
 var app = express.createServer(express.logger());
 
+function readFileContents(fileName) {
+  var buf = new Buffer(fs.readFileSync(fileName));
+  return buf.toString();
+}
+
+
 app.get('/', function(request, response) {
-  response.send(buffer.toString(fs.readFileSync(fileName, "utf-8")));
+  response.send(readFileContents(fileName));
 });
 
 var port = process.env.PORT || 5000;
